0093825111078298f73c216f6d66f8bf1e8ede8f
maxwellonah <117352916+maxwellonah@users.noreply.github.com>
2023-09-08 12:32:25 -0700

    Add files via upload

---
+++ IT/2/audio.php
@@ -0,0 +1,49 @@
+<link rel="stylesheet" href="css/modals.css">
+    <section class="mt-0 text-light">
+        <div class="container ">
+            <div class="row text-center">
+                <div class="col-md-12">
+                    <h2>ARTIST</h2>
+                    <img src="images\SPEAKER.jpg" class="img-fluid" alt="podcast" id="podcast">
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="mt-5 text-light">
+        <div class="container b-3">
+        <div class="row">
+            <div class="col-sm-3 mb-3 mb-sm-0">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-md-4">
+                                <img src="" alt="">
+                            </div>
+                            <div class="col-md-8"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-3">
+                <div class="card">
+                    <div class="card-body">
+                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-3">
+                <div class="card">
+                    <div class="card-body">
+                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-3">
+                <div class="card">
+                    <div class="card-body">
+                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>

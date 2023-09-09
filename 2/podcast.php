8bb36c9c6cade9cf62d0c4e120734c7fb011091b
maxwellonah <maxwellonah25@gmail.com>
2023-09-08 23:29:47 -0700

    second commit

---
+++ 2/podcast.php
@@ -0,0 +1,27 @@
+<link rel="stylesheet" href="css/modals.css">
+    <section class="mt-0 text-light">
+        <div class="container ">
+            <div class="row text-center">
+                <div class="col-md-12">
+                    <h2>PODCAST</h2>
+                    <img src="images/MIC STUDIO.jpg" class="img-fluid" alt="podcast" id="podcast">
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="mt-5 text-light">
+        <div class="container b-3">
+            <div class="row mt-5 text-center">
+                <div class="col-md-3"><img src="images\podcasts\analyticOfFire.png" alt=""><br>Analytic Of Fire</div>
+                <div class="col-md-3"><img src="images\podcasts\atlanticInterview.png" alt=""><br>Atlantic Interview</div>
+                <div class="col-md-3"><img src="images\podcasts\LostInCulture.png" alt=""><br>Lost In Culture</div>
+                <div class="col-md-3"><img src="images\podcasts\Purapur Expirience.png" alt=""><br>Purapur Expirience</div>
+            </div>
+            <div class="row mt-5 text-center">
+                <div class="col-md-3"><img src="images\podcasts\SlipTheRepeat.png" alt=""><br>Slip The Repeat</div>
+                <div class="col-md-3"><img src="images\podcasts\spaceInDepth.png" alt=""><br>Space In Depth</div>
+                <div class="col-md-3"><img src="images\podcasts\TheCrowdFolk.png" alt=""><br>The Crowd Folk</div>
+                <div class="col-md-3"><img src="images\podcasts\YoungPodcast.png" alt=""><br>Young Podcast</div>
+            </div>
+        </div>
+    </section>
